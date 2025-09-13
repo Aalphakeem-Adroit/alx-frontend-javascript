8b63a682ff92e7caee0ef3b96adf5f1b1c8be34c
Aalphakeem-Adroit <alphakeem12@gmail.com>
2025-09-14 00:08:11 +0100

    Extending the Teacher Interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -19,6 +19,24 @@ const teacher3: Teacher = {
 
 console.log(teacher3);
 
+// Extending the Teacher Interface
+interface Directors extends Teacher {
+  numberOfReports: number; // new required property
+}
+
+// Using the Directors Interface
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
+
+
+
 // Add teacher to an array
 const teacherList: Teacher[] = [teacher3];
 
