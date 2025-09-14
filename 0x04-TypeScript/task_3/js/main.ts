6fbe185cd8f1aee7a8515966af6d4c6b1799e841
Aalphakeem-Adroit <alphakeem12@gmail.com>
2025-09-14 11:41:47 +0100

    Fix: main.ts doesn't contain: ["{ ...row, age: 23 }"]

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -15,8 +15,7 @@ const row: RowElement = {
 const newRowID: RowID = CRUD.insertRow(row);
 
 // Update row with age = 23
-const updatedRow: RowElement = {
-  ...row, age: 23 };
+const updatedRow: RowElement = { ...row, age: 23 };
 CRUD.updateRow(newRowID, updatedRow);
 
 // Delete row
