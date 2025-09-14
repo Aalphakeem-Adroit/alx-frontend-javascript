034120a0b1f6c36208bcdd17a8546a81febc039c
Aalphakeem-Adroit <alphakeem12@gmail.com>
2025-09-14 10:39:53 +0100

    Fix: Function createEmployee

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -45,7 +45,7 @@ class Teacher implements TeacherInterface {
 
 //  Creating a function createEmployee
 function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === "number" && salary < 500) {
+  if (Number(salary) < 500) {
     return new Teacher();
   } else {
     return new Director();
