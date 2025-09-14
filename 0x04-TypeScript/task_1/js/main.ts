15a00e35fa6335e4b1b2c880a26439daf3019de2
Aalphakeem-Adroit <alphakeem12@gmail.com>
2025-09-14 10:34:35 +0100

    Task 5: Advanced Types Part 1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -20,6 +20,31 @@ const teacher3: Teacher = {
 
 console.log(teacher3);
 
+// Add teacher to an array
+const teacherList: Teacher[] = [teacher3];
+
+// Render a table
+const table = document.createElement("table");
+const tableBody = document.createElement("tbody");
+
+// Loop through teacher3 and create rows
+teacherList.forEach((teacher3) => {
+  const row = document.createElement("tr");
+
+  const nameCell = document.createElement("td");
+  nameCell.textContent = teacher3.firstName;
+
+  const locationCell = document.createElement("td");
+  locationCell.textContent = teacher3.location;
+
+  row.appendChild(nameCell);
+  row.appendChild(locationCell);
+  tableBody.appendChild(row);
+});
+
+table.appendChild(tableBody);
+document.body.appendChild(table);
+
 
 // 2. Extending the Teacher class
 // Extending the Teacher Interface
@@ -91,41 +116,3 @@ const student = new StudentClass({ firstName: "John", lastName: "Doe" });
 console.log(student.displayName());   // John
 console.log(student.workOnHomework()); // Currently working
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Add teacher to an array
-const teacherList: Teacher[] = [teacher3];
-
-// Render a table
-const table = document.createElement("table");
-const tableBody = document.createElement("tbody");
-
-// Loop through studentsList and create rows
-teacherList.forEach((teacher3) => {
-  const row = document.createElement("tr");
-
-  const nameCell = document.createElement("td");
-  nameCell.textContent = teacher3.firstName;
-
-  const locationCell = document.createElement("td");
-  locationCell.textContent = teacher3.location;
-
-  row.appendChild(nameCell);
-  row.appendChild(locationCell);
-  tableBody.appendChild(row);
-});
-
-table.appendChild(tableBody);
-document.body.appendChild(table);
---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,62 @@
+// 5. Advance Types Part 1
+// Creating the DirectorInterface
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+// Creating the TeacherInterface
+interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+}
+
+// Creating the Director Class that will implement the DirectorInterface
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return "Working from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Getting a coffee break";
+  }
+
+  workDirectorTasks(): string {
+    return "Getting to director tasks";
+  }
+}
+
+// Creating the Teacher Class that will implement the TeacherInterface
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return "Cannot work from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Cannot have a break";
+  }
+
+  workTeacherTasks(): string {
+    return "Getting to work";
+  }
+}
+
+//  Creating a function createEmployee
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === "number" && salary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+}
+
+// Example usage
+const emp1 = createEmployee(200);
+const emp2 = createEmployee(1000);
+const emp3 = createEmployee("500");
+
+console.log(emp1); // Teacher
+console.log(emp2); // Director
+console.log(emp3); // Director
